232298ed8c9fc3e7bbb130084f2f17a41cf56317
Dishant <dishantmehra20041@gmail.com>
2024-01-03 20:50:23 +0530

    controller setup

--- src/utils/asyncHandeler.js
+++
@@ -1,8 +0,0 @@
-const asyncHandler = (requestHandler) => {
-    return (req, res, next) => {
-        Promise.resolve(requestHandler(res, req, next)).catch((e) => next(e))
-    }
-}
-
-
-export { asyncHandler };
