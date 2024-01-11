1ca8848789ec1d270d8b08b918294404d1a2ac2a
Dishant <dishantmehra20041@gmail.com>
2024-01-11 21:07:00 +0530

    created pipelines for getting channel information and subscribers count

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -301,6 +301,72 @@ const udpateUserCoverImage = asyncHandler(async (req, res) => {
         .json(new ApiResponse(200, user, "Cover image updated successfully"));
 })
 
+
+const getUserChannelProfile = asyncHandler(async (req, res) => {
+    const { userName } = req.params;
+    if (!userName?.trim()) {
+        throw new ApiError(400, "User name is missing");
+    }
+    const channel = await User.aggregate([
+        {
+            $match: {
+                userName: userName?.toLowerCase()
+            }
+        },
+        {
+            $lookup: {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "channel",
+                as: "subscribers"
+            }
+        },
+        {
+            $lookup: {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "subscriber",
+                as: "subscribedTo"
+            }
+        },
+        {
+            $addFields: {
+                subscribersCount: {
+                    $size: "$subscribers"
+                },
+                channelsSubscribedToCount: {
+                    $size: "$subscribedTo"
+                },
+                isSubscribed: {
+                    $cond: {
+                        if: { $in: [req.user?._id, "$subscribers.subscriber"] },
+                        then: true,
+                        else: false
+                    }
+                }
+            }
+        },
+        {
+            $project: {
+                fullName: 1,
+                userName: 1,
+                subscribersCount: 1,
+                channelsSubscribedToCount: 1,
+                avatar: 1,
+                coverImage: 1,
+                isSubscribed: 1
+            }
+        }
+    ]);
+
+    if (channel?.length == 0) {
+        throw new ApiError(400, "Channel does not exist");
+    }
+
+    return res.status(200)
+        .json(new ApiResponse(200, channel[0]), "User channel fetched successfully");
+})
+
 export {
     registerUser,
     loginUser,
@@ -310,5 +376,6 @@ export {
     getCurrentUser,
     updateAccountDetails,
     udpateUserAvatar,
-    udpateUserCoverImage
+    udpateUserCoverImage,
+    getUserChannelProfile
 };
